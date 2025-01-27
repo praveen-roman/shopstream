0bcfe7bf463918e01fd0b8b9e8af63b6d0ddb786
praveen-roman <praveen07praveen07@gmail.com>
2025-01-27 09:29:44 -0800

    first commit

--- joes_project/settings.py
+++ joes_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
  
 INSTALLED_APPS = [
+  
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,7 +44,6 @@ INSTALLED_APPS = [
  
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
  
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'joes_project.urls'
  
 TEMPLATES = [
@@ -121,27 +123,21 @@ USE_TZ = True
  
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
  
-
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-#    Add this for production:
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#    Additional locations for static files
-STATICFILES_DIRS = [
-       os.path.join(BASE_DIR, 'static'),
-   ]
-
+ 
 MEDIA_URL='/images/'
 MEDIA_ROOT=BASE_DIR/'static'
  
-# STATICFILES_DIRS=[
-# 	BASE_DIR/'static'
-# ]
+STATICFILES_DIRS=[
+	BASE_DIR/'static'
+]
+STATICFILES_DIRS = [
+       os.path.join(BASE_DIR, 'static'),]
+
  
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
  
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
- 
--- joes_project/urls.py
+++ joes_project/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for joes_project project.
-
+"""joes_project URL Configuration
+ 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+ 
 from django.contrib import admin
 from django.urls import path,include
  
