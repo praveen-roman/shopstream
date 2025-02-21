b590531a2e7ac621ee042598b042c802a3135b50
praveen-roman <praveen07praveen07@gmail.com>
2025-02-20 19:46:32 -0800

    muki commit

--- praveen/settings.py
+++ praveen/settings.py
@@ -86,9 +86,6 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': 'root',
         'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
     }
 }
 
